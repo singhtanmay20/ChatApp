cb3f8afb47efc1ac2446c5a8fc612fa40d7361f7
singhtanmay20 <singhtanmay20@gmail.com>
2019-02-12 04:09:28 -0500

    Add files via upload

---
+++ src/com/Chat/ChatApp/OnlineUsers.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Chat.ChatApp;
+
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+public class OnlineUsers extends JFrame 
+{
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPane;
+	private JList list;
+
+	public OnlineUsers() 
+	{
+		setType(Type.UTILITY);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(200, 320);
+		setTitle("Online Users");
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 0;
+		gbc_list.gridy = 0;
+		JScrollPane p = new JScrollPane();
+		p.setViewportView(list);
+		contentPane.add(p, gbc_list);
+		list.setFont(new Font("Verdana", 0, 24));
+	}
+
+	public void update(String[] users) 
+	{
+		list.setListData(users);
+	}
+
+}
---
+++ src/com/Chat/ChatApp/server/ServerClient.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Chat.ChatApp.server;
+
+import java.net.InetAddress;
+
+public class ServerClient
+{
+
+	public String name;
+	public InetAddress address;
+	public int port;
+	private final int ID;
+	public int attempt = 0;
+
+	public ServerClient(String name, InetAddress address, int port, final int ID) 
+	{
+		this.name = name;
+		this.address = address;
+		this.port = port;
+		this.ID = ID;
+	}
+	
+	public int getID() 
+	{
+		return ID;
+	}
+
+}
